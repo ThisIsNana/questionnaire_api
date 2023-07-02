8df4038fb3a75d63438592a0095fc6a328e0abe7
ThisIsNana <pw10279@gmail.com>
2023-07-02 20:42:01 +0800

    初次設置

---
+++ src/main/java/com/example/questionnaire_api/QuestionnaireApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionnaire_api/QuestionnaireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
